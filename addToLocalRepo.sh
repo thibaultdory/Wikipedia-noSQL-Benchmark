cbaa136f6671162e0d3009a72cdfe8e8cb00deed
toflames <dory.thibault@gmail.com>
2011-02-20 15:41:31 +0100

    HBase 0.90.0 and maven resources correction

--- addToLocalRepo.sh
+++ addToLocalRepo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mvn install:install-file -Dfile=./lib/apache-cassandra-0.6.10.jar -DgroupId=org.apache.cassandra -DartifactId=cassandra -Dversion=0.6.10 -Dpackaging=jar
-mvn install:install-file -Dfile=./lib/hbase-0.20.6.jar -DgroupId=org.apache.hbase -DartifactId=hbase -Dversion=0.20.6 -Dpackaging=jar
+mvn install:install-file -Dfile=./lib/hbase-0.90.0.jar -DgroupId=org.apache.hbase -DartifactId=hbase -Dversion=0.90.0 -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/terrastore-javaclient-2.1.jar -DgroupId=terrastore-javaclient -DartifactId=terrastore-javaclient -Dversion=2.1 -Dpackaging=jar
